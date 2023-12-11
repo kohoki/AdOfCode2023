b3d142873d6f92b8c2f5791814733884b80b094d
Ralf Bonnick <ralfbonnick@gmx.net>
2023-12-11 20:54:49 +0100

    Solution day 1.0

--- day1.js
+++ day1.js
@@ -2,4 +2,22 @@ import { longString } from "./input.mjs";
 
 const inputFromFile = longString();
 
-console.log("Egon:", inputFromFile);
+let solution = inputFromFile
+  .replace(/[a-zA-Z]*/g, "")
+  .match(/\d+/g)
+  .map((num) => {
+    if (num.length > 2) {
+      return parseInt(num[0] + num.slice(-1), 10);
+    } else if (num.length === 1) {
+      return parseInt(num + num, 10);
+    } else {
+      return parseInt(num);
+    }
+  });
+  
+let sum = 0;
+solution.forEach((element) => {
+  sum += element;
+});
+
+console.log("Egon:", sum);
